719bc1197c9c5f7cc42858f338b5ee0d24c76eda
berryboylb <phemmynesce4life@gmail.com>
2023-08-17 08:51:47 +0100

    @chore: added svg assets

---
+++ assets/svgs/index.ts
@@ -0,0 +1,10 @@
+import Logo from "./644fcc5ebedd367c4e1434e5_logo.svg";
+import Star from "./6456b4a6447a6f38380a070a_star.svg";
+import ArrowRight from "./6456b69e861910267eb9421c_arrow-right.svg";
+import Play from "./6456bfba447a6f9eec0afa24_play.svg";
+import Bed from "./645900c47064075f330daf09_bed.svg";
+import Shower from "./645900c4c19e76cf54cd7a46_shower.svg";
+import Size from "./645900c400413f45b43620cb_size.svg";
+import Plus from "./6458f91bc9526d6a3cdcbc22_plus.svg";
+import WhiteLogo from "./644fcc5d198ee4989c3a9122_logo-white.svg";
+export { Logo, Star, ArrowRight, Play, Bed, Shower, Size, Plus, WhiteLogo };
