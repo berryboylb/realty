b555f9ba2c8083a6d597dbc693b3131a389dbf59
berryboylb <phemmynesce4life@gmail.com>
2023-08-17 08:54:50 +0100

    @chore: added jpegs

---
+++ assets/jpgs/index.ts
@@ -0,0 +1,35 @@
+import Condo from "./condo.jpg";
+import BedJPG from "./645b92e54e7e786524b0f1d9_property-01.jpg";
+import BigBed from "./6457643a9178cb3ff9362ca3_spacejoy-op9evfg3qww-unsplash-p-500.jpg";
+import Mirror from "./6457643a5a0c74f52525ee86_spacejoy-GNs831kqdoM-unsplash-p-1080.jpg";
+import Chair from "./6457644c6ea1db04504cd131_stephanie-harvey-PPA6wsuedeM-unsplash-p-500.jpg";
+import Property1 from "./645b92e54e7e786524b0f1d9_property-01.jpg";
+import Property2 from "./645b92ed6fad9c36b637c49d_property-02.jpg";
+import Property3 from "./645b92f4b620b371d7d27a3d_property-03.jpg";
+import Property4 from "./645b92fc6d79631a93ec24be_property-04.jpg";
+import Review1 from "./645ba5a149a601589986b898_testimonial 01.jpg";
+import Review2 from "./645ba5a12d80b3abe4a3966d_testimonial 02.jpg";
+import Post1 from "./645badd20c976617d2727593_post 01.jpg"
+import Post2 from "./645bae2cda50f50faac09640_post 02.jpg"
+import Post3 from "./645bae3d7b4a0bc0677b1444_post 03.jpg";
+import Post4 from "./645bae53060be1b9dc8c66fc_post 04.jpg";
+import Space from "./space.jpg";
+
+export {
+  Condo,
+  BedJPG,
+  BigBed,
+  Mirror,
+  Chair,
+  Property1,
+  Property2,
+  Property3,
+  Property4,
+  Review1,
+  Review2,
+  Post1,
+  Post2,
+  Post3,
+    Post4,
+  Space
+};
