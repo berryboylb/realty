f1cac0a4bf21eb9373646edbd15df20935e900ae
berryboylb <phemmynesce4life@gmail.com>
2023-09-13 03:46:47 +0100

    @chore: fixed url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { env } from "@/env.mjs";
+// import { env } from "@/env.mjs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,6 +16,6 @@ export function formatDate(input: string | number): string {
   });
 }
 
-export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
-}
+// export function absoluteUrl(path: string) {
+//   return `${env.NEXT_PUBLIC_APP_URL}${path}`;
+// }
